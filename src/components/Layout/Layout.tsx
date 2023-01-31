6cefce5e3880058c7314596b12bbb6dc07e92f7a
Waheed <a.waheed3738@gmail.com>
2023-01-31 22:12:48 +0500

    add layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "@/components/Navbar/Navbar";
+interface Props {
+    children: React.ReactNode;
+}
+ const Layout : React.FC<Props> = ({ children }) => {
+    return (
+        <>
+            <Navbar/>
+        <main>{children}</main>
+        </>
+    );
+};
+export default Layout
